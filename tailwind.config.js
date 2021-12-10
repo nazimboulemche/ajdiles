3b1d89e49d2c3957aaed1a77e128c0d27dff5239
nazimboulemche <70896734+nazimboulemche@users.noreply.github.com>
2021-12-10 15:09:54 +0100

    add

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+      margin: {
+
+        'ml-72':'52rem',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
